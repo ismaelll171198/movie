cb5fb2ef4f6cc7af3ae71c9ff7754a7df26a045c
Paola <adrianapaola186@gmail.com>
2017-05-26 14:20:25 +0200

    controlador

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root :to => "statics#index"
+  get '/about' => 'static#about', :as => :about
   get 'statics/index'
 
   get 'statics/about'
 
 	resources :peliculas
   get 'peliculas/index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
