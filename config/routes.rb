685fc233a21d9ad42f9122cbbc7e478607d1b3d2
Paola <adrianapaola186@gmail.com>
2017-06-08 09:35:01 +0200

    añdiendo administracion

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 	resources :countrys
 	resources :comentarios
 	resources :lenguages
+  resources :settings
   # resources :peliculas do
   #   resources :comentarios
   # end
