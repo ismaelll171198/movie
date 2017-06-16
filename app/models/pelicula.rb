3b214b3473cba371353f1ce5a33b7a8322d0a8fd
Paola <adrianapaola186@gmail.com>
2017-06-16 13:55:55 +0200

    añadir peliculas

--- app/models/pelicula.rb
+++ app/models/pelicula.rb
@@ -1,3 +1,4 @@
 class Pelicula < ApplicationRecord
 	has_many :comentarios, dependent: :destroy
+	validates :name, :url, :director, :year, presence:true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608084915) do
+ActiveRecord::Schema.define(version: 20170616081804) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -77,6 +77,8 @@ ActiveRecord::Schema.define(version: 20170608084915) do
     t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "director"
+    t.text "url"
   end
 
   create_table "programas", force: :cascade do |t|
